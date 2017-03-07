8f27acf88dedf171e1fc8acf0792146830253535
Mohammad Mohibul Hasan <mhpeash.ewu@hotmail.com>
2017-03-07 14:23:01 +0600

    completed till 8no videos name formatters

---
+++ APIDemo/App_Start/WebApiConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.AccessControl;
+using System.Web.Http;
+using Newtonsoft.Json.Serialization;
+
+namespace APIDemo
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+          //  config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
+            //config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            //config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+    }
+}
---
+++ APIDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using EmployeeDataAccess;
+
+
+namespace APIDemo.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IEnumerable<tbl_Employee> Get()
+        {
+            using (API_DatabaseEntities  db=new API_DatabaseEntities())
+            {
+               
+                
+                    return db.tbl_Employee.ToList();
+                
+                
+            }
+        }
+
+        public HttpResponseMessage Get( int id)
+        {
+            using (API_DatabaseEntities db = new API_DatabaseEntities())
+            {
+                var dbvalue = db.tbl_Employee.FirstOrDefault(a=>a.ID==id);
+                if (dbvalue != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, dbvalue);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with"+ id.ToString()+"not found");
+                }
+            }
+        }
+
+
+        public HttpResponseMessage Post( [FromBody] tbl_Employee employee )
+        {
+            using (API_DatabaseEntities db=new API_DatabaseEntities())
+            {
+                try
+                {
+                    db.tbl_Employee.Add(employee); //tbl_Employee is an object of DbSet<tbl_Employee> tbl_Employee
+                    db.SaveChanges();
+                    var message = Request.CreateResponse(HttpStatusCode.Created, employee);
+                    message.Headers.Location = new Uri(Request.RequestUri + employee.ID.ToString());
+                    return message;
+                }
+                catch (Exception ex)
+                {
+
+                   return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+                }
+                
+                
+            }
+        }
+
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                using (API_DatabaseEntities db = new API_DatabaseEntities())
+                {
+
+                    var checkeidexistance = db.tbl_Employee.FirstOrDefault(v => v.ID == id);
+                    if (checkeidexistance == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with id" + id.ToString() +" " +"Not found");
+                    }
+
+                    else
+                    {
+                        db.tbl_Employee.Remove(checkeidexistance);
+                        db.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK);
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+    }
+}
